842b78d1e34ac63469086eea56ee7c9be5e7880d
wasan <wasanawwad2020@gmail.com>
2024-04-11 20:26:49 +0300

    Deploy To Server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import Appinit from './src/Modules/app.router.js';
 
 dotenv.config();
 const app = express()
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 Appinit(app,express);
 
--- src/Modules/app.router.js
+++ src/Modules/app.router.js
@@ -2,8 +2,10 @@ import connectDB from '../../DB/Connection.js';
 import MassegeRouter from './Massege/Massege.Router.js'
 import AuthRouter from './Auth/Auth.Router.js'
 import UserRouter from './User/User.Router.js'
+import cors from 'cors';
 const Appinit = (app,express)=>{
     app.use(express.json());
+    app.use(cors());
     connectDB();
     app.use('/message',MassegeRouter);
     app.use('/auth',AuthRouter);
